d49012e4011c6f8d3a3b1ba99294430be96a9189
Ernst-Paul ten Brinke <eptb@schubergphilis.com>
2018-10-17 23:25:25 +0200

    doc

--- ci/scripts/run_tests.sh
+++ ci/scripts/run_tests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# These set options modify the shell's default behavior to cause any errors or unset variables to stop script execution and to print each command as they are executed. 
 set -e -u -x
 
 mv dependency-cache/node_modules hello_hapi
